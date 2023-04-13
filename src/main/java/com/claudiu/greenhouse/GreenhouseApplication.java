4d2a2dc35032b59f2f7ec03cee8eaffbf8551482
claudiuilie <claudiu.ilie0322@gmail.com>
2023-04-13 11:41:04 +0300

    first commit

--- src/main/java/com/claudiu/greenhouse/GreenhouseApplication.java
+++ src/main/java/com/claudiu/greenhouse/GreenhouseApplication.java
@@ -20,7 +20,7 @@ public class GreenhouseApplication {
         final Console console = new Console();
         Context pi4j = Pi4J.newAutoContext();
 
-        int dataPinNum = 4;
+		int dataPinNum = 0xff;
         String traceLevel = "info";
         console.title("<-- The Pi4J V2 Project Extension  -->", "DHT22_App");
         String helpString = " parms:  -d  data GPIO number  -t trace " +
