739f528441da884d775b32083acd5f8ab937313b
claudiuilie <claudiu.ilie0322@gmail.com>
2023-04-13 16:42:04 +0300

    first commit

--- dht22.py
+++ dht22.py
@@ -1,35 +1,13 @@
-import Adafruit_DHT
-from gpiozero import InputDevice
-import json
+import Adafruit_DHT as dht
+from time import sleep
 
-# Set sensor type : Options are DHT11,DHT22 or AM2302
-sensor = Adafruit_DHT.DHT22
+# Define the sensor type and GPIO pin
+#Set DATA pin
+DHT = 4
 
-# Initialize a list to store the data for each sensor
-sensor_data = []
-
-# Iterate over each pin on the Raspberry Pi
-for pin in range(4, 28):
-    device = None
-    try:
-        # Attempt to read the data from the DHT22 sensor on this pin
-        device = InputDevice(pin, pull_up=False)
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-        # If data was successfully read, append it to the sensor_data list
-        if humidity is not None and temperature is not None:
-            sensor_data.append({
-                "pin": pin,
-                "temperature": temperature,
-                "humidity": humidity
-            })
-    except Exception as e:
-        # If an error occurred, print a message indicating which pin had the error
-        print(f"Error reading from pin {pin}: {str(e)}")
-    finally:
-        # Close the device to avoid errors on subsequent reads
-        if device is not None:
-            device.close()
-
-# Print the sensor data as a JSON object
-print(json.dumps(sensor_data))
+while True:
+    #Read Temp and Hum from DHT22
+    h,t = dht.read_retry(dht.DHT22, DHT)
+    #Print Temperature and Humidity on Shell window
+    print('Temperature: {:.1f}°C  Humidity: {:.1f}%'.format(t,h))
+    sleep(3) #Wait 5 seconds and read again
