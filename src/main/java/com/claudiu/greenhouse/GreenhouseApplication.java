8c5bf70e5eeebd631f3bf11746c84383adac3968
claudiuilie <claudiu.ilie0322@gmail.com>
2023-04-13 11:37:05 +0300

    first commit

--- src/main/java/com/claudiu/greenhouse/GreenhouseApplication.java
+++ src/main/java/com/claudiu/greenhouse/GreenhouseApplication.java
@@ -1,75 +1,75 @@
 package com.claudiu.greenhouse;
 
+import com.claudiu.greenhouse.pojo.DHT22Sensor;
 import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
 import com.pi4j.exception.LifecycleException;
-import com.pi4j.io.gpio.digital.*;
-import com.pi4j.platform.Platforms;
 import com.pi4j.util.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
+
 @SpringBootApplication
 public class GreenhouseApplication {
 
-	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(GreenhouseApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(GreenhouseApplication.class, args);
 
-		final Console console = new Console();
-		Context pi4j = Pi4J.newAutoContext();
+        final Console console = new Console();
+        Context pi4j = Pi4J.newAutoContext();
 
-		int dataPinNum = 4;
-		String traceLevel = "info";
-		console.title("<-- The Pi4J V2 Project Extension  -->", "DHT22_App");
-		String helpString = " parms:  -d  data GPIO number  -t trace " +
-				" \n -t  trace values : \"trace\", \"debug\", \"info\", \"warn\", \"error\" \n " +
-				" or \"off\"  Default \"info\"";
-		Signal.handle(new Signal("INT"), new SignalHandler() {
-			public void handle(Signal sig) {
-				System.out.println("Performing ctl-C shutdown");
-				try {
-					pi4j.shutdown();
-				} catch (LifecycleException e) {
-					e.printStackTrace();
-				}
-				// Thread.dumpStack();
-				System.exit(2);
-			}
-		});
+        int dataPinNum = 4;
+        String traceLevel = "info";
+        console.title("<-- The Pi4J V2 Project Extension  -->", "DHT22_App");
+        String helpString = " parms:  -d  data GPIO number  -t trace " +
+                " \n -t  trace values : \"trace\", \"debug\", \"info\", \"warn\", \"error\" \n " +
+                " or \"off\"  Default \"info\"";
+        Signal.handle(new Signal("INT"), new SignalHandler() {
+            public void handle(Signal sig) {
+                System.out.println("Performing ctl-C shutdown");
+                try {
+                    pi4j.shutdown();
+                } catch (LifecycleException e) {
+                    e.printStackTrace();
+                }
+                // Thread.dumpStack();
+                System.exit(2);
+            }
+        });
 
-		for (int i = 0; i < args.length; i++) {
-			String o = args[i];
-			if (o.contentEquals("-d")) {
-				String a = args[i + 1];
-				dataPinNum = Integer.parseInt(a);
-				i++;
-			} else if (o.contentEquals("-t")) {
-				String a = args[i + 1];
-				i++;
-				traceLevel = a;
-				if (a.contentEquals("trace") | a.contentEquals("debug") | a.contentEquals("info") | a.contentEquals("warn") | a.contentEquals("error") | a.contentEquals("off")) {
-					console.println("Changing trace level to : " + traceLevel);
-				} else {
-					console.println("Changing trace level invalid  : " + traceLevel);
-					System.exit(41);
-				}
-			} else if (o.contentEquals("-h")) {
-				console.println(helpString);
-				System.exit(41);
-			} else {
-				console.println("  !!! Invalid Parm " + o);
-				console.println(helpString);
-				System.exit(43);
-			}
-		}
-		DHT22 sensor = new DHT22(pi4j, console, dataPinNum,  traceLevel);
-		sensor.readAndDisplayData();
+        for (int i = 0; i < args.length; i++) {
+            String o = args[i];
+            if (o.contentEquals("-d")) {
+                String a = args[i + 1];
+                dataPinNum = Integer.parseInt(a);
+                i++;
+            } else if (o.contentEquals("-t")) {
+                String a = args[i + 1];
+                i++;
+                traceLevel = a;
+                if (a.contentEquals("trace") | a.contentEquals("debug") | a.contentEquals("info") | a.contentEquals("warn") | a.contentEquals("error") | a.contentEquals("off")) {
+                    console.println("Changing trace level to : " + traceLevel);
+                } else {
+                    console.println("Changing trace level invalid  : " + traceLevel);
+                    System.exit(41);
+                }
+            } else if (o.contentEquals("-h")) {
+                console.println(helpString);
+                System.exit(41);
+            } else {
+                console.println("  !!! Invalid Parm " + o);
+                console.println(helpString);
+                System.exit(43);
+            }
+        }
 
-		 console.waitForExit();
-		pi4j.shutdown();
+        DHT22Sensor sensor = new DHT22Sensor(pi4j, console, dataPinNum, traceLevel);
+        sensor.readAndDisplayData();
 
+        console.waitForExit();
+        pi4j.shutdown();
 
 
 //		Platforms platforms = pi4j.platforms();
@@ -105,6 +105,6 @@ public class GreenhouseApplication {
 //		// shutdown Pi4J
 //		System.out.println("ATTEMPTING TO SHUTDOWN/TERMINATE THIS PROGRAM");
 //		pi4j.shutdown();
-	}
+    }
 
 }
