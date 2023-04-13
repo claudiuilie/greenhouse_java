e40432a2597b614578e0796a6a28456c185bf6ff
claudiuilie <claudiu.ilie0322@gmail.com>
2023-04-13 16:35:09 +0300

    first commit

--- dht22.py
+++ dht22.py
@@ -1,54 +1,13 @@
-import Adafruit_DHT
-import RPi.GPIO as GPIO
-import json
+import Adafruit_DHT as dht
+from time import sleep
 
-# Set sensor type : Options are DHT11,DHT22 or AM2302
-sensor = Adafruit_DHT.DHT22
+# Define the sensor type and GPIO pin
+#Set DATA pin
+DHT = 4
 
-# Dictionary of GPIO pin numbers and their corresponding BCM numbers
-PIN_MAP = {
-    3: 2,
-    5: 3,
-    7: 4,
-    8: 14,
-    10: 15,
-    11: 17,
-    12: 18,
-    13: 27,
-    15: 22,
-    16: 23,
-    18: 24,
-    19: 10,
-    21: 9,
-    22: 25,
-    23: 11,
-    24: 8,
-    26: 7
-}
-
-# Initialize a list to store the data for each sensor
-sensor_data = []
-
-# Set up the GPIO pins
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-# Iterate over each pin in the PIN_MAP dictionary
-for pin, bcm_pin in PIN_MAP.items():
-    try:
-        # Attempt to read the data from the DHT22 sensor on this pin
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, bcm_pin)
-
-        # If data was successfully read, append it to the sensor_data list
-        if humidity is not None and temperature is not None:
-            sensor_data.append({
-                "pin": pin,
-                "temperature": temperature,
-                "humidity": humidity
-            })
-    except Exception as e:
-        # If an error occurred, print a message indicating which pin had the error
-        print(f"Error reading from pin {pin}: {str(e)}")
-
-# Print the sensor data as a JSON object
-print(json.dumps(sensor_data))
+while True:
+    #Read Temp and Hum from DHT22
+    h,t = dht.read_retry(dht.DHT22, DHT)
+    #Print Temperature and Humidity on Shell window
+    print('Temperature: {:.1f}°C  Humidity: {:.1f}%'.format(t,h))
+    sleep(3) #Wait 5 seconds and read again
